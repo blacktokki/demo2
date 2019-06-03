c8e882891630f71e581652fb971ae245bffa0fc4
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-06-04 00:47:21 +0900

    Revert "release/1.0.0 (#17)"
    This reverts commit 887d69cde6523d3475cedb2bab85483624b44b48.

--- demo2/src/main/java/com/example/demo2/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2.service;
-
-import org.springframework.ui.Model;
-
-public interface BoardService {
-	public void content(Model model);
-	public void save(Model model);
-	public void write(Model model);
-}
