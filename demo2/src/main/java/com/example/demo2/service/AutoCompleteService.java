d421cc18329a2526649c3536cc233d72523870dd
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-06-18 01:52:41 +0900

    Revert "release/1.0.1 (#18)"
    This reverts commit b02cf40261a044a257692d5a8e2427ad81d68524.

--- demo2/src/main/java/com/example/demo2/service/AutoCompleteService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2.service;
-
-import org.json.simple.JSONObject;
-import org.springframework.ui.Model;
-
-public interface AutoCompleteService {
-	public JSONObject search(Model model);
-}
--- demo2/src/main/java/com/example/demo2/service/JobService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2.service;
-
-import org.springframework.ui.Model;
-
-public interface JobService {
-	public void jobs(Model model);
-	
-}
